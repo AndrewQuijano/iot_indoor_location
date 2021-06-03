73e536c9b2604184058fb5f332bd095cd60ba746
AndrewQuijano <andrewquijano92@gmail.com>
2021-06-02 21:51:23 -0400

    No longer need to use tshark script. Will have monitor not require arguments to switch mode

--- set_monitor.sh
+++ set_monitor.sh
@@ -4,14 +4,17 @@
 # Obtain Wireless Interface Information, Change it moinotor mode/managed mode
 
 INT=$(sudo iw dev | sed -n '2 p' | cut -d ' ' -f2)
-if [[ 'ON' == $1 ]]; then
+MODE=$(sudo iw dev | grep monitor | cut -d ' ' -f2)
+if [[ 'monitor' == $1 ]]; then
 	echo "Monitor Mode - ON"
-	sudo ip link set $INT down
- 	sudo iw $INT set monitor none
- 	sudo ip link set $INT up
-else
-	echo "Monitor Mode - OFF"
 	sudo ip link set $INT down
 	sudo iw $INT set type managed
 	sudo ip link set $INT up 
+	echo "Monitor Mode - OFF"
+else
+	echo "Monitor Mode - OFF"
+	sudo ip link set $INT down
+ 	sudo iw $INT set monitor none
+ 	sudo ip link set $INT up
+	echo "Monitor Mode - ON"
 fi
